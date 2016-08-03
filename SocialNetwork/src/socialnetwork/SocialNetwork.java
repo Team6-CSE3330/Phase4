b0b9b4d92cfec327165c4e1776e095391f316420
ameerak01 <ameera.khan@mavs.uta.edu>
2016-08-03 16:50:20 -0500

    Database connection?

--- SocialNetwork/src/socialnetwork/SocialNetwork.java
+++ SocialNetwork/src/socialnetwork/SocialNetwork.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package socialnetwork;
-
+import java.sql.*;
+import java.io.*;
+import javax.swing.*;
 /**
  *
  * @author AmeeraK
@@ -14,8 +16,14 @@ public class SocialNetwork {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws SQLException {
+         final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+         
+         Connection conn = DriverManager.getConnection("jdbc:mysql://address=(protocol=tcp)(host=omega.uta.edu) (port=3306) (user=axk2904) (password=April123)/axk2904"); 
+         System.out.println(conn.getSchema());
+         
+        
+        
     }
     
 }
